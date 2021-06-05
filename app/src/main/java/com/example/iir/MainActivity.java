c4d1e4435d5bf00c3fd4cac880d43b6bebec6eda
Sreehari M V <sreeharimv10@gmail.com>
2021-06-05 14:37:50 +0530

    Implicit Intent Receiver

---
+++ app/src/main/java/com/example/iir/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.iir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        if (uri != null)
+        {
+            String uri_string = "URL: " + uri.toString();
+            TextView textView = (TextView) findViewById(R.id.text);
+            textView.setText(uri_string);
+        }
+
+    }
+}
